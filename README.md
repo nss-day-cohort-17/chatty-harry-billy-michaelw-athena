7ce1901e3790c246f993eb74d40f159049c0b4b6
Michael Watson <m.watson.tn@gmail.com>
2016-12-08 12:12:50 -0600

    delete messg progress js-mw

--- README.md
+++ README.md
@@ -16,11 +16,11 @@ Check out the [simple wireframe](https://app.moqups.com/chortlehoort/uGBbLbK46Y/
 
 ### Navigation bar
 
-1. Create an element to serve as the navigation bar for your application.
-1. Create an element to hold the logo for your application. It can be as simple as text, but if you want to find an image, that's fine.
+<!-- 1. Create an element to serve as the navigation bar for your application. -->
+<!-- 1. Create an element to hold the logo for your application. It can be as simple as text, but if you want to find an image, that's fine.
 1. Create a input field for a user to enter in a message.
 1. Add an event listener for "keypress" and detect when then return key has been pressed in the message field.
-1. When return key is detected, you'll create a new message (*see details below*).
+1. When return key is detected, you'll create a new message (*see details below*). -->
 1. Create a button to clear all messages.
 1. When the user clicks the clear messages button, all current chat messages should be removed from the application.
 1. If there are no messages, then the clear messages button should be disabled (*see example above*).
@@ -28,7 +28,7 @@ Check out the [simple wireframe](https://app.moqups.com/chortlehoort/uGBbLbK46Y/
 
 ### Options
 
-1. Create two checkboxes below the message input field. One labeled "Dark theme" and the other labeled "Large text".
+<!-- 1. Create two checkboxes below the message input field. One labeled "Dark theme" and the other labeled "Large text". -->
 1. When the user clicks on the dark theme checkbox, change the background color of your application to a dark gray, and the font color for messages should be white(ish)... you pick.
 1. If the user unchecks the box, the background color should change back to white with black text for messages.
 
--- main.js
+++ main.js
@@ -4,47 +4,43 @@ var messageTextBox = document.getElementById("enterMessageTextBox");
 var storingTheMessage;
 messageTextBox.addEventListener('keypress', function(e) {
 
-                          if(e.code === "Enter" ) {
-                            var typedContent="";
-                            var newVariable="";
+    if(e.code === "Enter" ) {
+      var typedContent="";
+      var newVariable="";
 
-                                // console.log("you hit the enter button")
-                            // we want to take the text in the #messsagebox
-                                typedContent += document.getElementById("enterMessageTextBox").value
+          // console.log("you hit the enter button")
+      // we want to take the text in the #messsagebox
+          typedContent += document.getElementById("enterMessageTextBox").value
 
-                                //
-                                console.log("this is typed content", typedContent)
-                                //
-                                // the text in the #messagebox will be saved- assign to a variable
-                                // we can grab the value what is inside the html  and set this to the variable var typedContent
-                                //
-                                //
+     // this creates the container for typed content
+     //
 
-                           // this creates the container for typed content
-                           //
+      newVariable += `<div class="messageBubble">
+                           <div class="words">${typedContent} </div>
+                             <button class="deleteThisMessage">buttonMe</button>
+                       </div>`
 
-                            newVariable += `<div class="messageBubble">
-                                                 <div class="words">${typedContent} </div>
-                                                   <button class="deleteThisMessage">buttonme</button>
-                                             </div>`
+    //   // // //  and put iti into the #messagewrapper
 
-                          //   // // //  and put iti into the #messagewrapper
+          document.getElementById("messageWrapper").innerHTML +=  newVariable
 
-                                document.getElementById("messageWrapper").innerHTML +=  newVariable
-
-                                    clearContent()
-
-
-                          }
-
-                })
+              clearContent()
+}
+})
+// var oneMessage = getElementById("messageBubble").innerHTML.value;
+document.querySelector("body").addEventListener("click", deleteOneMessage);
+function deleteOneMessage(e) {
+  console.log(e)
+  if (e.target.className === "deleteThisMessage") {
+    e.parent = "";
+}
+}
 // this clears the typed content and we rule
 function clearContent()
   {
    document.getElementById("enterMessageTextBox").value = "";
 }
 
-
-deleteThisMessage.addEventListener('click', function(e) {
-    document.getElementById("messagebubble")
-}
+// deleteThisMessage.addEventListener('click', function(e) {
+//     document.getElementById("messagebubble")
+// }
